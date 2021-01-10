28fb04965c192579499c79acfbd8c9d60c77039b
David Xu <dxaviud@gmail.com>
2021-01-10 16:16:00 -0500

    refactoring

--- adder.py
+++ adder.py
@@ -1,21 +1,12 @@
 #Simple adding program using tkinter gui
 
-from tkinter import Tk
-from tkinter import Button
-from tkinter import Label
-from tkinter import Entry
+from tkinter import Tk, Button, Label, Entry
 
 class Adder:
 
-    def display(self):
+    def __init__(self):
 
         root = Tk(screenName="adder")
-
-        def redden_entry(entry):
-            entry.config(background="red")
-
-        def whiten_entry(entry):
-            entry.config(background="white")
 
         first_number_label = Label(root, text = "First Number")
         first_number_label.grid(sticky="E", padx=5, pady=5, row=0, column=0)
@@ -32,23 +23,27 @@ class Adder:
         result_label = Label(root, text="Result")
         result_label.grid(sticky="EW", padx=5, pady=5, columnspan=2, row=3, column=0)
 
-        def add_event():
+        def redden(entry):
+            entry.config(background="red")
 
+        def whiten(entry):
+            entry.config(background="white")
+
+        def add():
             error_message = ""
             first_number_text = first_number_entry.get()
             second_number_text = second_number_entry.get()
             try:
                 first_number = float(first_number_text)
-                whiten_entry(first_number_entry)
+                whiten(first_number_entry)
             except ValueError:
-                redden_entry(first_number_entry)
+                redden(first_number_entry)
                 error_message += "First number invalid.\n"   
-
             try:
                 second_number = float(second_number_text)
-                whiten_entry(second_number_entry)
+                whiten(second_number_entry)
             except ValueError:
-                redden_entry(second_number_entry)
+                redden(second_number_entry)
                 error_message += "Second number invalid."
             
             if error_message != "":
@@ -58,12 +53,12 @@ class Adder:
                 
             result_label.config(text=str(result))
 
-        add_button = Button(root, text="Add", command=add_event)
+        add_button = Button(root, text="Add", command=add)
         add_button.grid(sticky="EW", row=2, column=0, columnspan=2, padx=5, pady=5)        
 
         root.mainloop()
 
 
 if __name__ == '__main__':
-    app = Adder()
-    app.display()
+    Adder()
+
